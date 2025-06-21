eed8d1057f80720efe47db4861f13cc342a5a932
ZuraArabidze <z.arabidzee@gmail.com>
2025-06-21 13:31:50 +0400

    Add GetProducts and GetProductById endpoints, ProductNotFoundException, fix category inserting into database

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException(Guid id) : base($"Product with the Id : {id} not found!") 
+    {
+
+    }
+}
